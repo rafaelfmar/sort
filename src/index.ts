b67227ff632921143a384b36004c1620b4d3a183
Rafael Marques <rafael.fmarques@outlook.com>
2021-09-25 20:25:16 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0, 7]);
-const sorter = new Sorter(numbersCollection);
+const numbersSorter = new Sorter(numbersCollection);
 
-sorter.sort();
+numbersSorter.sort();
 
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('XzyUiaHj');
+const charactersSorter = new Sorter(charactersCollection);
+
+charactersSorter.sort();
+
+console.log(charactersCollection.data);
