17019c9501bc82060797100dc0ddc0da5b9166f8
Rafael Marques <rafael.fmarques@outlook.com>
2021-09-25 21:03:33 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0, 7]);
 const numbersSorter = new Sorter(numbersCollection);
@@ -15,3 +16,14 @@ const charactersSorter = new Sorter(charactersCollection);
 charactersSorter.sort();
 
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+
+linkedList.print();
